f7cc55523602bdf8423e02b09f80a13092c1109d
Ayuub Mohammad <ayuub.ultra.clean.786@proton.me>
2026-03-22 23:46:32 +0500

    Fix: Unroll DSC node loops, inject AD wait barriers, enforce explicit AGDLP objects, and expand Systems documentation boundaries natively

--- DSC/Configurations/StudentConfig.ps1
+++ DSC/Configurations/StudentConfig.ps1
@@ -19,76 +19,214 @@ Configuration StudentBaseline {
 
     Node $AllNodes.NodeName {
         
-        # Proof-of-life
-        File TestFolder {
-            DestinationPath = 'C:\TEST'
-            Type            = 'Directory'
-            Ensure          = 'Present'
+        # 0. Base Windows Config
+        Computer ServerName {
+            Name = $Node.ComputerName
         }
-        File TestFile {
-            DestinationPath = 'C:\TEST\test.txt'
-            Type            = 'File'
-            Ensure          = 'Present'
-            Contents        = 'Proof-of-life: DSC created this file.'
-            DependsOn       = '[File]TestFolder'
+
+        TimeZone SetTimeZone {
+            IsSingleInstance = 'Yes'
+            TimeZone = $Node.TimeZone
+        }
+
+        Service W32TimeService {
+            Name = 'W32Time'
+            State = 'Running'
+            StartupType = 'Automatic'
         }
 
         # 1. Root Domain
         ADDomain BoltonDomain {
             DomainName                = $Node.DomainName
-            IsSingleInstance          = 'Yes'
-            DomainAdministratorCredential = $DomainAdminCredential
+            DomainNetBIOSName         = $Node.DomainNetBIOSName
+            Credential                = $DomainAdminCredential
             SafemodeAdministratorPassword = $DsrmCredential
         }
 
-        # 2. Data-Driven OU Structure 
-        foreach ($ou in $Node.OUs) {
-            ADOrganizationalUnit "OU_$($ou.Name)" {
-                Name      = $ou.Name
-                Path      = $ou.Path
-                DependsOn = '[ADDomain]BoltonDomain'
-            }
+        ADWaitForDomain WaitForBolton {
+            DomainName = $Node.DomainName
+            DomainUserCredential = $DomainAdminCredential
+            DependsOn = '[ADDomain]BoltonDomain'
+            RetryIntervalSec = 20
+            RetryCount = 30
         }
 
-        # 3. Data-Driven RBAC Groups
-        foreach ($group in $Node.Groups) {
-            $ouName = $group.Path.Split(',')[0].Split('=')[1]
-            ADGroup "Group_$($group.Name)" {
-                GroupName  = $group.Name
-                Path       = $group.Path
-                Category   = 'Security'
-                GroupScope = 'Global'
-                DependsOn  = if ($ouName -eq 'bolton') { '[ADDomain]BoltonDomain' } else { "[ADOrganizationalUnit]OU_$ouName" }
-            }
+        # 2. OU Structure
+        ADOrganizationalUnit OU_Users {
+            Name      = 'Users'
+            Path      = "DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
+        }
+        
+        ADOrganizationalUnit OU_Computers {
+            Name      = 'Computers'
+            Path      = "DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
         }
 
-        # 4. Data-Driven Identity Baseline
-        foreach ($user in $Node.Users) {
-            $ouName = $user.Path.Split(',')[0].Split('=')[1]
-            ADUser "User_$($user.Name)" {
-                UserName    = $user.Name
-                Path        = $user.Path
-                Password    = $UserCredential
-                DependsOn   = if ($ouName -eq 'bolton') { '[ADDomain]BoltonDomain' } else { "[ADOrganizationalUnit]OU_$ouName" }
-            }
-
-            ADGroupMember "Member_$($user.Name)" {
-                GroupName = $user.Group
-                Members   = $user.Name
-                DependsOn = @("[ADGroup]Group_$($user.Group)", "[ADUser]User_$($user.Name)")
-            }
+        ADOrganizationalUnit OU_BoltonAdmins {
+            Name      = 'BoltonAdmins'
+            Path      = "DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
         }
 
-        # 5. Data-Driven Group Policy Creation
-        foreach ($gpo in $Node.GPOs) {
-            GroupPolicy "GPO_$($gpo.Name)" {
-                Name   = $gpo.Name
-                Ensure = $gpo.Ensure
-                DependsOn = '[ADDomain]BoltonDomain'
-            }
+        ADOrganizationalUnit OU_Derby {
+            Name      = 'Derby'
+            Path      = "DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
         }
 
-        # 6. A* FGPP Requirements (Hard-coded structure representing Tier-0 Security)
+        ADOrganizationalUnit OU_Nottingham {
+            Name      = 'Nottingham'
+            Path      = "OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADOrganizationalUnit]OU_Derby'
+        }
+
+        ADOrganizationalUnit OU_DerbyAdmins {
+            Name      = 'DerbyAdmins'
+            Path      = "OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            DependsOn = '[ADOrganizationalUnit]OU_Derby'
+        }
+
+        # 3. RBAC Groups (AGDLP Model Implementation)
+        # Global Groups
+        ADGroup GG_BoltonAdmins {
+            GroupName  = 'GG-BoltonAdmins'
+            Path       = "OU=BoltonAdmins,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'Global'
+            DependsOn  = '[ADOrganizationalUnit]OU_BoltonAdmins'
+        }
+
+        ADGroup GG_DerbyAdmins {
+            GroupName  = 'GG-DerbyAdmins'
+            Path       = "OU=DerbyAdmins,OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'Global'
+            DependsOn  = '[ADOrganizationalUnit]OU_DerbyAdmins'
+        }
+
+        ADGroup GG_DerbyStaff {
+            GroupName  = 'GG-DerbyStaff'
+            Path       = "OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'Global'
+            DependsOn  = '[ADOrganizationalUnit]OU_Derby'
+        }
+
+        ADGroup GG_NottinghamStaff {
+            GroupName  = 'GG-NottinghamStaff'
+            Path       = "OU=Nottingham,OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'Global'
+            DependsOn  = '[ADOrganizationalUnit]OU_Nottingham'
+        }
+
+        # Domain Local Groups
+        ADGroup DLG_BoltonAdmins {
+            GroupName  = 'DLG-BoltonAdmins'
+            Path       = "OU=BoltonAdmins,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'DomainLocal'
+            DependsOn  = '[ADOrganizationalUnit]OU_BoltonAdmins'
+        }
+
+        ADGroup DLG_DerbyAdmins {
+            GroupName  = 'DLG-DerbyAdmins'
+            Path       = "OU=DerbyAdmins,OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Category   = 'Security'
+            GroupScope = 'DomainLocal'
+            DependsOn  = '[ADOrganizationalUnit]OU_DerbyAdmins'
+        }
+
+        # AGDLP Bindings
+        ADGroupMember Bind_BoltonAdmins {
+            GroupName = 'DLG-BoltonAdmins'
+            Members   = 'GG-BoltonAdmins'
+            DependsOn = @('[ADGroup]DLG_BoltonAdmins', '[ADGroup]GG_BoltonAdmins')
+        }
+
+        ADGroupMember Bind_DerbyAdmins {
+            GroupName = 'DLG-DerbyAdmins'
+            Members   = 'GG-DerbyAdmins'
+            DependsOn = @('[ADGroup]DLG_DerbyAdmins', '[ADGroup]GG_DerbyAdmins')
+        }
+
+        # 4. Identity Definitions
+        ADUser User_AdminJane {
+            UserName    = 'AdminJane'
+            Path        = "OU=BoltonAdmins,DC=bolton,DC=barmbuzz,DC=test"
+            Password    = $UserCredential
+            DependsOn   = '[ADOrganizationalUnit]OU_BoltonAdmins'
+        }
+        ADGroupMember Member_AdminJane {
+            GroupName = 'GG-BoltonAdmins'
+            Members   = 'AdminJane'
+            DependsOn = @('[ADGroup]GG_BoltonAdmins', '[ADUser]User_AdminJane')
+        }
+
+        ADUser User_JaneDoe {
+            UserName    = 'JaneDoe'
+            Path        = "OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Password    = $UserCredential
+            DependsOn   = '[ADOrganizationalUnit]OU_Derby'
+        }
+        ADGroupMember Member_JaneDoe {
+            GroupName = 'GG-DerbyStaff'
+            Members   = 'JaneDoe'
+            DependsOn = @('[ADGroup]GG_DerbyStaff', '[ADUser]User_JaneDoe')
+        }
+
+        ADUser User_BobSmith {
+            UserName    = 'BobSmith'
+            Path        = "OU=Nottingham,OU=Derby,DC=bolton,DC=barmbuzz,DC=test"
+            Password    = $UserCredential
+            DependsOn   = '[ADOrganizationalUnit]OU_Nottingham'
+        }
+        ADGroupMember Member_BobSmith {
+            GroupName = 'GG-NottinghamStaff'
+            Members   = 'BobSmith'
+            DependsOn = @('[ADGroup]GG_NottinghamStaff', '[ADUser]User_BobSmith')
+        }
+
+        # 5. GPO Deployments (Meaningful Objects & Links)
+        Gpo GPO_Baseline {
+            Name = 'BaselineSecurity'
+            Ensure = 'Present'
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
+        }
+        GpoLink Link_Baseline {
+            GpoName = 'BaselineSecurity'
+            Target = 'DC=bolton,DC=barmbuzz,DC=test'
+            Ensure = 'Present'
+            DependsOn = '[Gpo]GPO_Baseline'
+        }
+
+        Gpo GPO_Derby {
+            Name = 'DerbyRegionalPolicy'
+            Ensure = 'Present'
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
+        }
+        GpoLink Link_Derby {
+            GpoName = 'DerbyRegionalPolicy'
+            Target = 'OU=Derby,DC=bolton,DC=barmbuzz,DC=test'
+            Ensure = 'Present'
+            DependsOn = @('[Gpo]GPO_Derby', '[ADOrganizationalUnit]OU_Derby')
+        }
+        
+        Gpo GPO_Nottingham {
+            Name = 'NottinghamOperations'
+            Ensure = 'Present'
+            DependsOn = '[ADWaitForDomain]WaitForBolton'
+        }
+        GpoLink Link_Nottingham {
+            GpoName = 'NottinghamOperations'
+            Target = 'OU=Nottingham,OU=Derby,DC=bolton,DC=barmbuzz,DC=test'
+            Ensure = 'Present'
+            DependsOn = @('[Gpo]GPO_Nottingham', '[ADOrganizationalUnit]OU_Nottingham')
+        }
+
+        # 6. A* FGPP Security Control Implementation
         ADFineGrainedPasswordPolicy PwdPolicyAdmins {
             Name               = 'FGPP-ITAdmins'
             Precedence         = 10
@@ -100,13 +238,13 @@ Configuration StudentBaseline {
             LockoutDuration    = '00:30:00'
             LockoutObservationWindow = '00:15:00'
             LockoutThreshold   = 3
-            DependsOn          = '[ADDomain]BoltonDomain'
+            DependsOn          = '[ADWaitForDomain]WaitForBolton'
         }
 
         ADFineGrainedPasswordPolicySubject PwdPolicyAdminsSubject {
             PolicyName = 'FGPP-ITAdmins'
-            Subjects   = 'GG-IT-Admins'
-            DependsOn  = @('[ADFineGrainedPasswordPolicy]PwdPolicyAdmins', '[ADGroup]Group_GG-IT-Admins')
+            Subjects   = 'GG-BoltonAdmins'
+            DependsOn  = @('[ADFineGrainedPasswordPolicy]PwdPolicyAdmins', '[ADGroup]GG_BoltonAdmins')
         }
     }
 }
--- DSC/Data/AllNodes.psd1
+++ DSC/Data/AllNodes.psd1
@@ -4,29 +4,13 @@
             NodeName   = 'localhost'
             Role       = 'DC'
             DomainName = 'bolton.barmbuzz.test'
-            
-            OUs = @(
-                @{ Name = 'Users'; Path = 'DC=bolton,DC=barmbuzz,DC=test' }
-                @{ Name = 'Computers'; Path = 'DC=bolton,DC=barmbuzz,DC=test' }
-                @{ Name = 'IT-Admins'; Path = 'DC=bolton,DC=barmbuzz,DC=test' }
-                @{ Name = 'Derby'; Path = 'DC=bolton,DC=barmbuzz,DC=test' }
-                @{ Name = 'Nottingham'; Path = 'OU=Derby,DC=bolton,DC=barmbuzz,DC=test' }
-            )
-
-            Groups = @(
-                @{ Name = 'GG-Staff'; Path = 'OU=Users,DC=bolton,DC=barmbuzz,DC=test' }
-                @{ Name = 'GG-IT-Admins'; Path = 'OU=IT-Admins,DC=bolton,DC=barmbuzz,DC=test' }
-            )
-
-            Users = @(
-                @{ Name = 'JohnDoe'; Path = 'OU=Users,DC=bolton,DC=barmbuzz,DC=test'; Group = 'GG-Staff' }
-                @{ Name = 'AdminJane'; Path = 'OU=IT-Admins,DC=bolton,DC=barmbuzz,DC=test'; Group = 'GG-IT-Admins' }
-            )
-
-            GPOs = @(
-                @{ Name = 'Baseline Security'; Ensure = 'Present' }
-                @{ Name = 'Derby Regional Policy'; Ensure = 'Present' }
-            )
+            DomainNetBIOSName = 'BOLTON'
+            ComputerName = 'Bolton-DC01'
+            InterfaceAlias_Internal = 'Ethernet'
+            IPv4Address_Internal = '192.168.0.10'
+            ForestMode = 'WinThreshold'
+            DomainMode = 'WinThreshold'
+            TimeZone = 'GMT Standard Time'
         }
     )
 }
